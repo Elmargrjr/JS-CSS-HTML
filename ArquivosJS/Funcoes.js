c431b613a45f0cf707e329869472b4a941356701
Elmar Júnior <junigrossl@gmail.com>
2026-03-30 20:53:47 -0300

    Exclusão ultima linha

--- ArquivosJS/Funcoes.js
+++ ArquivosJS/Funcoes.js
@@ -12,5 +12,3 @@ function verificarIdade(idade) {
         console.log ("Você é menor de idade");
     }
 }
-
-//VAR respeita o escopo de função, ou seja, se for declarada dentro de uma função, ela só existe dentro dessa função. Já o LET respeita o escopo de bloco, ou seja, se for declarada dentro de um bloco (como um if ou um loop), ela só existe dentro desse bloco. O VAR pode ser redeclarada e reatribuída, enquanto o LET não pode ser redeclarada, mas pode ser reatribuída. O VAR é hoisted (içado) para o topo do seu escopo, mas não é inicializado até a linha onde é declarada. O LET também é hoisted, mas não é inicializado e fica em um "temporal dead zone" até a linha onde é declarada.
