e041ebf2072bc8f75f00ae73deecfc40fc765c8c
Elmar Júnior <junigrossl@gmail.com>
2026-03-29 21:05:19 -0300

    Final do documento

--- ArquivosJS/Dados primitivosJS.js
+++ ArquivosJS/Dados primitivosJS.js
@@ -71,6 +71,58 @@ let StringInvalidaParaNumero = Number(StringInvalida);
 console.log (StringInvalida);
 console.log (typeof StringInvalidaParaNumero, StringInvalidaParaNumero);
 
+//Váriaveis
+//let,const e var
+//let deixa voce mudar o valor da variável e até o tipo dela
+//const nao deixa, o valor é constante
+
+const pi = 3.14;
+
+const horasdoDia = 24;
+
+const nomeCompleto = "Cristiano Ronaldo";
+
+console.log (pi);
+console.log (horasdoDia);
+console.log (nomeCompleto);
+
+let endereço2 = 12;
+let endereço2s = String(endereço2);
+
+console.log ("O endereço de João é: " + endereço2);
+console.log (typeof endereço2, endereço2);
+console.log (typeof endereço2s, endereço2s);
+
+//Var nao tem escopo de bloco, ou seja, ela pode ser acessada fora do bloco onde foi declarada, o que pode levar a problemas de segurança e bugs no código. Por isso, é recomendado usar let ou const em vez de var para declarar variáveis em JavaScript.
+//Let tem escopo de bloco, ou seja, ela só pode ser acessada dentro do bloco onde foi declarada, o que ajuda a evitar problemas de segurança e bugs no código. Por isso, é recomendado usar let ou const em vez de var para declarar variáveis em JavaScript.
+
+for (var i = 0; i < 5; i++) {
+    var nome3 = "João";
+    console.log (i);
+}
+
+console.log (nome3); // Isso vai funcionar, pois var tem escopo de função, não de bloco
+
+for (let i = 0; i < 5; i++) {
+    let nome4 = "Maria";
+    console.log (nome4);
+}
+
+//console.log(nome4); // Isso vai gerar um erro, pois let tem escopo de bloco
 
 
+//Operadores
+
+let count = 0;
+count++; // Incremento, soma 1
+console.log (count);
+
+console.log ("é igual", 5 == "5");
+// == compara valor (com coerção de tipo)
+console.log ("é estritamente igual", 5 === "5");
+// === compara valor e tipo (sem coerção de tipo)
+
+
+console.log("é igual?", 1 == 1 ? "Sim" : "Não");
+//condicional ? Verdadeiro : Falso
 
